878f3fc21959be0a6d5c54cf1892a69c919b3160
PC <nishthamishra189@gmail.com>
2021-08-23 00:20:42 +0530

    First Commit uploading all files

---
+++ WebBrowser/src/webBrowser.java
@@ -0,0 +1,36 @@
+
+
+import javax.swing.*;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.web.WebView;
+import javafx.scene.Scene;
+import javafx.application.Platform;
+
+public  class webBrowser extends JFrame implements Runnable{
+	
+	private JFXPanel panel;
+	
+	//webBrowser(){
+		public void run() {
+		setTitle("Search");
+		setBounds(0,0,1950,1050);
+		setVisible(true);
+		
+		panel =new JFXPanel();
+		add(panel);
+		
+		Platform.runLater(() ->{
+		WebView view=new WebView();
+		view.getEngine().load("http://www.google.com");
+		 panel.setScene(new Scene(view));
+		});
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new webBrowser());
+
+	}
+
+}
